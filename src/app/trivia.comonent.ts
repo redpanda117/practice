c872d9d653c746d23b8fb4feae381843e6ffc1ea
Tina Nguyen <nguyentina2005@yahoo.com>
2018-01-18 18:22:09 -0500

    background img work

---
+++ src/app/trivia.comonent.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector:'trivia',
+    template: '<h2>This is the containter for the questions</h2>'
+})
+
+
+export class TriviaComponent {
+
+}
